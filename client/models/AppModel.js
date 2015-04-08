099b9210682eb98bbc1af07cc3f151c6b6250f86
kurtbartholomew <siever246@gmail.com>
2015-04-08 15:04:26 -0700

    Pass all App tests

--- client/models/AppModel.js
+++ client/models/AppModel.js
@@ -16,6 +16,12 @@ var AppModel = Backbone.Model.extend({
     params.library.on('play', function(song){
       this.set('currentSong', song);
     }, this);
+
+    params.library.on('enqueue', this.enqueueSong, this);
+  },
+
+  enqueueSong: function(song) {
+    this.get('songQueue').add(song);
   }
 
 });
