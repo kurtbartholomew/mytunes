e994676177c47b45b70b595a0ff791df106db699
kurtbartholomew <siever246@gmail.com>
2015-04-09 11:37:20 -0700

    Update unit test for LibraryEntryViewSpec to reflect current functionality

--- client/views/LibraryEntryView.js
+++ client/views/LibraryEntryView.js
@@ -7,11 +7,7 @@ var LibraryEntryView = Backbone.View.extend({
 
   events: {
     'click': function() {
-      this.model.play();
-      // only call enqueue if currentSong is not itself
-      if (!this.model.get('beingPlayed')) {
-        this.model.enqueue();
-      }
+      this.model.enqueue();
     }
   },
 
