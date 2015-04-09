241452e5cf5861167ece57bc08aa7e46bc99654b
kurtbartholomew <siever246@gmail.com>
2015-04-09 10:18:32 -0700

    Add ability to dequeue a song

--- client/views/SongQueueEntryView.js
+++ client/views/SongQueueEntryView.js
@@ -1,8 +1,17 @@
 // SongQueueEntryView.js - Defines a backbone view class for the song queue entries.
 var SongQueueEntryView = Backbone.View.extend({
 
+  events: {
+    'click .dequeue' : 'dequeueSong'
+  },
+
+  dequeueSong: function() {
+    this.model.dequeue();
+  },
+
   template: _.template("<span><%= artist %></span> - "+
-                       "<span><%= title %></span>"),
+                       "<span><%= title %></span> "+
+                       "<a class=\"dequeue\" href=\"#\">Remove</a>"),
 
   render: function(){
     return this.$el.html(this.template(this.model.toJSON()));
